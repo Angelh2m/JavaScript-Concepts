87364cf419ae206f1f4292435628d4a086531c22
AngelH2m <angelh2m@icloud.com>
2018-04-04 08:58:40 -0400

    regex

---
+++ regex2.js
@@ -0,0 +1,42 @@
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+.[{()\^$|?*+
+
+coreyms.com
+
+mac
+bat
+cat
+pat
+
+coreyMrshafer@gmail.com
+corey.Mrshafer@university.edu
+corey-Mrs-hafer@my-work.net
+
+
+https://www.google.com
+https://corey.com
+https://youtube.com
+https://www.nasa.com
+
+
+321-555-4321
+123.555.1234
+800.555.1234
+900.555.1234
+123#555#1234
+
+Mr. Schafer
+Mr Smith
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+
+
+Escape character =  \.
